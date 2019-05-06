92233b53c4a5ae01cc2f1fcb2166ffb1dec567a3
DelayedBrush3 <agustinferegrinof@hotmail.com>
2019-05-06 11:23:01 -0500

    middleware add

---
+++ app/Http/Middleware/sa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class sa
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user()->role != 'sa'){
+            return redirect()->back()->with('flash', 'No tienes privilegios para está acción.');
+        }
+
+        return $next($request);
+    }
+}
